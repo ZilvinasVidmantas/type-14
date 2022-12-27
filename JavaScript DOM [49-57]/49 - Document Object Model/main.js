1e588a97a107c92cfc6b2274f8773df28479406b
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-12-27 17:01:55 +0200

    feat(49): task answer

--- JavaScript DOM [49-57]/49 - Document Object Model/main.js
+++ JavaScript DOM [49-57]/49 - Document Object Model/main.js
@@ -24,9 +24,39 @@ Užduotis:
         display: inline-block
 
   Naudojamos funkcijos:
+    A)
     document.querySelector(<pagalKąIeškote>) - suranda esantį elementą
+    B)
     document.createElement(<kąKuriate>) - sukuria elementą
+    C)
     target.style.<cssSavybėPavadinimas> = <cssSavybėsReikšmė>; - nustato <target> CSS savybę
+    D)
     target.addEventListener(<įKokįĮvykįReaguoti>, <kokiąFunkcijąVykdyti>) - uždeda klausiklį
+    E)
     target.appendChild(<KąPridėti>) - Prideda  elementą <KąPridėti>) į <target> elemento galą.
 */
+
+// 1. A)
+const btnAddSquare = document.querySelector('.js-btn-add-square');
+// 2. A)
+const squareContainer = document.querySelector('.js-square-container');
+
+const addSquare = () => {
+  // 4. B)
+  const square = document.createElement('span');
+  // C)
+  square.style.background = '#BADA55';
+  // C)
+  square.style.height = '100px';
+  // C)
+  square.style.width = '100px';
+  // C)
+  square.style.display = 'inline-block';
+  square.style.marginRight = '1rem';
+
+  // E)
+  squareContainer.appendChild(square);
+}
+
+// 3. D)
+btnAddSquare.addEventListener('click', addSquare);
