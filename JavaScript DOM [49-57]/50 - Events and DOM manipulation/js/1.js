cc2d5f7c716aa693994ba2e8a50577d3467dae6b
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-12-28 13:56:26 +0200

    feat(50): DOM tasks

---
+++ JavaScript DOM [49-57]/50 - Events and DOM manipulation/js/1.js
@@ -0,0 +1,11 @@
+// IIFE - Imediatly invoked function expression
+(() => {
+  const square = document.querySelector('.js-square-1');
+  const btn = document.querySelector('.js-btn-1');
+  
+  const changeSquareColor = () => {
+    square.style.background = 'red';
+  }
+  
+  btn.addEventListener('click', changeSquareColor);
+})()
