cc4f8325f1b4e2318f039967404dd650bea81f15
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2023-01-02 14:48:31 +0200

    feat(53): task

---
+++ JavaScript DOM [49-57]/53 - promises and async functions/main.js
@@ -0,0 +1,15 @@
+/*
+  Sukurti UI, kuriame vartotojas galėtų pasirinkti vartotoją, ir tuomet pagal pasirinktą
+  vartotoją matytūsi TIK tok vartotojo postai.
+
+
+  1. Atvaizdavimo darbai
+    1. Padaryti lentelės vaizdą pagal duomenis kuriuos planuoju parsisiųsti
+    2. select'o vaizdą, pagal duomenis kuriuos planuoju parsisiųsti 
+
+  2. Parsiuntimo darbai
+    1. Atsiųsti vartotojus
+    2. Suformuoti select'o pasirinkimus pagal parsiųstus vartotojus
+    3. Apdoroti ir išsitestuoti nesėkmės atvejį
+
+*/
