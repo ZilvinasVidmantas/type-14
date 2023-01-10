e51e0d86d27a1e48b963f094a1e32fdd5c844ee3
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2023-01-10 13:06:35 +0200

    init(59): type definition

--- TypeScript [58-72]/58 - TS įvadas/projekto kurimas paskaitos metu/src/main.ts
+++ TypeScript [58-72]/58 - TS įvadas/projekto kurimas paskaitos metu/src/main.ts
@@ -1,7 +1,9 @@
-import { a, b } from './numbers.js';
+const firstName: string = 'Opelijus';
+const age: number = 21;
+const numbers: number[] = [1, 1, 2, 3, 5, 8, 13, 21];
 
-const addMany = (...args: number[]): number => args.reduce((s, x) => s + x);
-
-console.log(addMany(a, b));
-console.log(addMany(a, b, b));
-console.log(addMany(4, 1, 5, 6, 7));
+type Person = {
+  name: string,
+  surname: string,
+  age: number,
+};
