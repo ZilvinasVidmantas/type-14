3775f402873b1fbec7e8742aecacc6bc045ca103
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2023-02-23 14:14:17 +0200

    feat(90): delete btn UI

--- React [73-91]/90 - CRUD - delete mutation loading edit data/client/src/pages/home-page/house-card.tsx
+++ React [73-91]/90 - CRUD - delete mutation loading edit data/client/src/pages/home-page/house-card.tsx
@@ -8,6 +8,7 @@ import {
 import Img from 'components/ui/img';
 import { useNavigate } from 'react-router-dom';
 import routes from 'navigation/routes';
+import DeleteIcon from '@mui/icons-material/Delete';
 import * as Styled from './styled';
 
 type HouseCardProps = HouseModel;
@@ -23,7 +24,23 @@ const HouseCard: React.FC<HouseCardProps> = ({
   const navigate = useNavigate();
 
   return (
-    <Stack sx={{ boxShadow: 4 }}>
+    <Stack sx={{ boxShadow: 4, position: 'relative' }}>
+      <Button
+        variant="contained"
+        color="error"
+        size="small"
+        sx={{
+          position: 'absolute',
+          right: 10,
+          top: 10,
+          minWidth: 'initial',
+          p: 0.5,
+        }}
+        onClick={() => console.log(`uždaugęs darysiu užklausą į jSON serverį per ApiService ir sulaukęs satsakymo per naujo atsiųsiu visus duomenis. mano id '${id}'`)}
+      >
+        <DeleteIcon />
+      </Button>
+
       <Img src={images[0]} alt="" sx={{ aspectRatio: '1.42', width: 1 }} />
       <Styled.HouseCardContent>
         <Box sx={{ flexGrow: 1 }}>
