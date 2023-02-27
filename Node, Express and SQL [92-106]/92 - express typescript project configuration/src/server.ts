1150dd53ff932a6135cc788db748ff761e284060
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2023-02-27 16:40:28 +0200

    feat(92): .env variables and configured ts-paths

--- Node, Express and SQL [92-106]/92 - express typescript project configuration/src/server.ts
+++ Node, Express and SQL [92-106]/92 - express typescript project configuration/src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import morgan from 'morgan';
+import config from 'config';
 
 const server = express();
+server.use(morgan('tiny'));
 server.use(express.static('public'));
 
-server.listen(5001, () => {
-  console.log('Server is running on http://localhost:5001');
+server.listen(config.server.port, () => {
+  console.log(`Server is running on ${config.server.address}`);
 });
