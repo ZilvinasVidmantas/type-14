2032b9c9f03a8daa2633aee4b4ead775174d3cab
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2023-03-01 16:55:28 +0200

    feat(94): delete request handler

--- Node, Express and SQL [92-106]/94 - REST queries and mutations/src/houses/index.ts
+++ Node, Express and SQL [92-106]/94 - REST queries and mutations/src/houses/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import getHouses from './queries/get-houses';
 import getHouse from './queries/get-house';
 import createHouse from './mutations/create-house';
+import deleteHouse from './mutations/delete-house';
 
 const housesRouter = express.Router();
 
@@ -15,9 +16,6 @@ housesRouter.patch('/:id', (req, res) => {
   res.send('updated one house');
 });
 
-// delete one
-housesRouter.delete('/:id', (req, res) => {
-  res.send('deleted one house');
-});
+housesRouter.delete('/:id', deleteHouse);
 
 export default housesRouter;
