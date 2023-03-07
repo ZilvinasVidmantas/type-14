c790ccb3cf38646e5bd5ae68f8e9fed4a585a6c4
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2023-03-07 13:03:35 +0200

    init(98): database schema and migrations

---
+++ Node, Express and SQL [92-106]/98 - database schema and migrations/src/houses/mutations/delete-house.ts
@@ -0,0 +1,30 @@
+import { RequestHandler } from 'express';
+import ServerSetupError from 'errors/server-setup-error';
+import handleRequestError from 'helpers/handle-request-error';
+import { houses } from 'houses/data';
+import HouseNotFoundError from 'houses/house-not-found-error';
+import { HouseModel } from 'houses/types';
+
+const deleteHouse: RequestHandler<
+  { id?: string },
+  HouseModel | ErrorResponse,
+  {},
+  {}
+> = (req, res) => {
+  const { id } = req.params;
+
+  try {
+    if (id === undefined) throw new ServerSetupError();
+
+    const foundHouseIndex = houses.findIndex((house) => String(house.id) === id);
+    if (foundHouseIndex === -1) throw new HouseNotFoundError(id);
+
+    const [foundHouse] = houses.splice(foundHouseIndex, 1);
+
+    res.status(200).json(foundHouse);
+  } catch (err) {
+    handleRequestError(err, res);
+  }
+};
+
+export default deleteHouse;
