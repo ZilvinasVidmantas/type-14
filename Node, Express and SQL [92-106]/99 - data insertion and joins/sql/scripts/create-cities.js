e0d5c2bb31cdf2f36acd3530f84d8888fa5000cb
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2023-03-08 17:06:36 +0200

    feat(99): created users script

--- Node, Express and SQL [92-106]/99 - data insertion and joins/sql/scripts/create-cities.js
+++ Node, Express and SQL [92-106]/99 - data insertion and joins/sql/scripts/create-cities.js
@@ -2,6 +2,7 @@ const dbData = require('../old-data/db.json');
 const { countryMap } = require('./create-countries');
 
 const { houses } = dbData;
+
 const insertionRows = houses
   .map(x => [countryMap[x.location.country], x.location.city])
   .map(([id, title]) => `(${id}, '${title}')`)
@@ -12,3 +13,17 @@ insert into city(countryId, title) values
 ${insertionRows};
 `
 console.log(insertionSQL)
+
+// TODO: suformuoti miestų struktūrą
+
+cityId = cityMap['Wychodne']['Poland']
+
+const cityMap = {
+  Wychodne: {
+    Poland: 1
+  },
+  Leliunai: {
+    Anyksciai: 25,
+    Moletai: 33
+  },
+}
