73983fb45988a81b1b95cc394bd3adee692495b0
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2023-03-10 13:51:33 +0200

    feat(101): first SQL request

--- Node, Express and SQL [92-106]/101 - express api with sql/src/houses/queries/get-houses.ts
+++ Node, Express and SQL [92-106]/101 - express api with sql/src/houses/queries/get-houses.ts
@@ -1,13 +1,23 @@
+import config from 'config';
 import { RequestHandler } from 'express';
 import { houses } from 'houses/data';
 import { HouseModel } from 'houses/types';
+import mysql from 'mysql2/promise';
 
 const getHouses: RequestHandler<
   {},
   HouseModel[],
   undefined,
   {}
-> = (req, res) => {
+> = async (req, res) => {
+  const connection = await mysql.createConnection(config.database);
+
+  const [queryResult] = await connection.query('SELECT * FROM ts14.house;');
+
+  console.log(queryResult);
+
+  connection.end();
+
   res.json(houses);
 };
 
